0e4e70b0bfa223176df2360dd53cce9e2e5a4bc5
kkidgamer <khaembagift@gmail.com>
2025-07-29 09:57:00 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import HomeComponents from './components/HomeComponents';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeComponents/>} />
+
+        </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/HomeComponents.jsx
@@ -0,0 +1,9 @@
+const HomeComponents = () => {
+  return (
+    <div >
+      <h1>Welcome to EMIS</h1>
+      <p>This is the home page of the EMIS application.</p>
+    </div>
+  );
+}
+export default HomeComponents;
